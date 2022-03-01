075053bc52481d87ff700087ae2a698386cf57c4
N93NT0T <75997866+N93NT0T@users.noreply.github.com>
2022-03-01 19:23:43 +0700

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -93,13 +93,8 @@ async def start(c, m, cb=False):
                 caption += f"__👁 Members Count:__ {channel.members_count}\n\n" if channel.members_count else ""
             else:
                 user = await c.get_users(int(chat_id)) 
-                caption += "**--Uploader Details:--**\n\n" 
-                caption += f"__🦚 First Name:__ `{user.first_name}`\n\n" 
-                caption += f"__🐧 Last Name:__ `{user.last_name}`\n\n" if user.last_name else "" 
-                caption += f"__👁 User Name:__ @{user.username}\n\n" if user.username else "" 
-                caption += f"__👤 User Id:__ `{user.id}`\n\n" 
-                caption += f"__💬 DC kontol ID:__ {user.dc_id}\n\n" if user.dc_id else ""
-
+                caption += "** Terus support channel ini, agar mimmin terus update. 😎**\n\n" 
+                
 
         await send_msg.delete()
         await msg.copy(m.from_user.id, caption=caption)
